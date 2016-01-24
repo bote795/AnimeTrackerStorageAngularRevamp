14009a26d2496f73462032fddc51f686353df063
bote795 <nbotell2@gmail.com>
2016-01-24 13:15:10 -0600

    add angular material for autocomplete
    was able to get autocomplete working with anilist and wrote up function
    to refresh token

--- js/anilistController.js
+++ js/anilistController.js
@@ -1,9 +1,17 @@
 app.controller('anilistController', function($scope,$http) {
 	$scope.PinCode="";
 	$scope.client_id = "";
-	$scope.client_secret = "";
-	$scope.access_token = "";
+	$scope.client_id = "bote795-nuwwf";
+	$scope.client_secret = "zDQRctaEZByOo0ybExLyybj1O";
 	$scope.anime="";
+	$scope.access_token;
+	if (typeof localStorage["token"] !== "undefined") {
+		$scope.access_token = JSON.parse(localStorage["token"])["access_token"];
+		if (new Date() > new Date(localStorage["token"]["expires"])) 
+		{
+			$scope.refreshToken();
+		};
+	};
 	$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 	$scope.saveCode= function() {
 		var params = {
@@ -28,6 +36,12 @@ app.controller('anilistController', function($scope,$http) {
 		.then(function(response) {
 			console.log(response.data);
 			$scope.access_token = response.data.access_token;
+			delete response.data["token_type"];
+			var t = new Date()
+			t.setHours(t.getHours()+ 1);
+			response.data["expires"] = t.getTime();
+			localStorage["token"] = JSON.stringify(response.data);
+			localStorage["refresh_token"] = JSON.stringify(response.data["refresh_token"]);
             console.log("response");
 	    }, 
 	    function(response) { // optional
@@ -36,20 +50,57 @@ app.controller('anilistController', function($scope,$http) {
 
 	    });
 	}
-	$scope.findAnime= function() {
-		
-		$http({
-		  url: 'https://anilist.co/api/anime/search/'+$scope.anime,
+	$scope.selected =function (item) {
+		alert(JSON.stringify(item));
+	}
+	/*
+		handles autocomplete for input anime
+	*/
+	$scope.querySearch= function(query) {
+		return $http({
+		  url: 'https://anilist.co/api/anime/search/'+query,
 		  method: 'GET',
 		  headers: {
 		  	'Authorization' : 'Bearer '+$scope.access_token
 		  }
 		})
+		.then(function(response){
+	      return response.data;
+	    },
+	    function(response) { // optional
+	       console.log("fail");
+	       console.log( response);
+
+	    });
+	}		
+	/*
+		will refresh the access_token
+	*/
+	$scope.refreshToken = function() {
+		var params = {
+            'grant_type': 'refresh_token',
+            'client_id':  $scope.client_id,
+            'client_secret': $scope.client_secret,
+            'refresh_token': JSON.parse(localStorage["refresh_token"]),
+		};
+
+		$http({
+		  url: 'https://anilist.co/api/auth/access_token',
+		  method: 'POST',
+		  data: $.param(params) // Make sure to inject the service you choose to the controller
+		})
+		/*
+			response data contains:
+			    access_token: "NR3M3vXgHK0kmluOcJVlRXvbGOg4yLhAVyf5If"
+			    token_type: "bearer"
+			    expires: 1414234981
+			    expires_in: 3600    aka an hour
+		*/
 		.then(function(response) {
 			console.log(response.data);
-			for (var i = 0; i < response.data.length; i++) {
-				console.log(response.data[i].title_english);
-			};
+			$scope.access_token = response.data.access_token;
+			delete response.data["token_type"];
+			localStorage["token"] = JSON.stringify(response.data);
             console.log("response");
 	    }, 
 	    function(response) { // optional
@@ -57,6 +108,5 @@ app.controller('anilistController', function($scope,$http) {
 	       console.log( response);
 
 	    });
-	}		
-	
+	}
 });
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngRoute','ui.sortable'])
+var app = angular.module('myApp', ['ngRoute','ui.sortable','ngMaterial'])
 .config( [
     '$compileProvider',
     function( $compileProvider )
