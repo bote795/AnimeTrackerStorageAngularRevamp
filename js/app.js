d24ad99901bdce04cbcb7330dd3d8d8af8daae17
bote795 <nbotell2@gmail.com>
2016-05-31 14:44:42 -0500

    snyc data to anilist
    check if list from chrome extension is up to date with anilist db. if
    its not then update entries or pull data from anilist and update local
    data.

--- js/app.js
+++ js/app.js
@@ -37,21 +37,16 @@ app.config(function ($routeProvider) {
         redirectTo: '/home'
     });
 });
+function resetNewEpFields(anime){
+    anime["isNewEpAvialable"]=0;
+    anime["newEpUrl"]="url";
+}
 /*
 when ap first run check for updates
 */
 app.run(function($rootScope, anilistFac) {
     var spintarget = document.getElementById('foo');
     var spinner = new Spinner().spin(spintarget);
-    userManager.load(function(user){\
-        if(user.providers.anilist){
-            anilistFac.registerObserverCallback(function(){
-
-                
-            });  
-        }
-      
-    });
     //check for updates then check for total eps
     var checkForUpdates = updates(function() {
         FindTotalEps(function() {
@@ -59,4 +54,49 @@ app.run(function($rootScope, anilistFac) {
             spinner.stop();            
         });
     });
+    userManager.load(function(user){
+        if(user.providers.anilist){
+            //retrieve anime List from chrome extension
+            animeDataManager.load(function(animelist){
+                //retrieve user list from anilist 
+                anilistFac.RetrieveUserList().then(function(list){
+                    //go through chrome extension anime
+                    animelist.forEach(function(anime,index){
+                        //if anime is from anilist 
+                        if('provider' in anime && anime.provider && anime.anilist)
+                        {
+                            //find anime from list that the user is watching
+                            var filterList = list.filter(function(val) {
+                                return val.anime.id == anime.id;
+                            })
+                            //make sure that we only got one
+                            if (filterList.length == 1){
+                                //make sure that anime is not equal to same episode
+                                if (filterList[0].episodes_watched != anime.ep) {
+                                    //console.log(anime.name,animelist[index],filterList[0].episodes_watched);
+                                    //server number is bigger 
+                                    if (filterList[0].episodes_watched > anime.ep) {
+                                        animelist[index].ep = filterList[0].episodes_watched;
+                                        resetNewEpFields(animelist[index]);
+                                    }
+                                    //local number is bigger must save that in anilist
+                                    else {  
+                                        anilistFac.editAnime({ep: anime.ep, id: anime.id}).then(function(response){
+                                            console.log("sucessfully updated", response)
+                                        });
+                                    }
+                                }
+                            } 
+                        }
+                    });
+                    //save anime list
+                    animeDataManager.save(animelist);
+                    checkForUpdates;
+                });
+            });
+        }
+      
+    });
+
+    
 });
